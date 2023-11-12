e1f0b6a88c79f830cfa179c37ba44995de8fa935
Porta <portalmaster137@gmail.com>
2023-11-13 00:13:11 +1000

    Init

---
+++ booter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Synopsys: This file is intended to run on a LiveCD Arch Linux system.
+# and is intended to install a base system to a specified disk, along with some
+# necessary packages and configuration files.
